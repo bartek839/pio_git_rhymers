f1530d0e7fb795ad1797c6920f349a34b22fd162
bartek839 <patoraban3@wp.pl>
2020-01-14 18:56:28 +0100

    pikpik

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -1,11 +1,13 @@
 package edu.kis.vh.nursery;
 
-public class FIFORhymer extends defaultCountingOutRhymer {
+public class FIFORhymer extends defaultCountingOutRhymer 
+{
 
 	public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
 	
 	@Override
-	public int countOut() {
+	public int countOut() 
+	{
 		while (!callCheck())
 			
 		temp.countIn(super.countOut());
